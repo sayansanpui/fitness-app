25d765796dcc70e29c10a89a6ad03ccac0628511
sayansanpui <sayansanpuicoding@gmail.com>
2025-04-14 21:39:00 +0530

    NavBar design done

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Poppins',
+        ),
+        home: const HomePage());
   }
 }
